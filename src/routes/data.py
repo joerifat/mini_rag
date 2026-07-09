06474fb50e2a7b0c149e22e8ca83d45f14d95b6e
youssef rifat <youssefrifat899@gmail.com>
2026-07-09 09:01:40 +0300

    refactor: Update file path generation in upload_file function

--- src/routes/data.py
+++ src/routes/data.py
@@ -22,8 +22,10 @@ async def upload_file(project_id: str,file: UploadFile,
 
     project_dir_path = ProjectController().get_project_path(project_id)
 
-    file_path= os.path.join(project_dir_path
-                            ,file.filename)
+    file_path, file_id = DataController.generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
     
     async with aiofiles.open(file_path ,"wb") as f:
         while chunk := await file.read(app_settings.File_Chunk_size):
